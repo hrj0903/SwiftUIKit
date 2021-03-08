645c1bf8fc405802c38959ca20409bcf71ce23b7
hrj0903 <heerij93@gmail.com>
2021-03-08 14:01:46 +0900

    SwiftUI Projects

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -21,8 +21,10 @@ class ViewController: UITableViewController {
         let urlString: String
         
         if navigationController?.tabBarItem.tag == 0 {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
         } else {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
         
