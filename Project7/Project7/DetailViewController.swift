289d449386a1d750e3e3621581476844e5096298
hrj0903 <heerij93@gmail.com>
2021-03-08 12:57:31 +0900

    SwiftUI Projects

---
+++ Project7/Project7/DetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DetailViewController.swift
+//  Project7
+//
+//  Created by hrj on 2021/03/07.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    var webView: WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let detailItem = detailItem else { return }
+        
+        let html = """
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style> body { font-size: 150%; } </style>
+        </head>
+        <body>
+        <h2 style="font-style:italic"><span style="font-family:Verdana,Geneva,sans-serif"><span style="font-size:18px">\(detailItem.body)</span></span></h2>
+        </body>
+        </html>
+        """
+        
+        webView.loadHTMLString(html, baseURL: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -9,26 +9,31 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
+    var savePetitions = [Petition]()
+    var filterPetitions = [Petition]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Info", style: .plain, target: self, action: #selector(promptForInfo))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain , target: self, action: #selector(filteredPetitions))
+        
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+                return
             }
         }
-    }
-    
-    func parse(json: Data) {
-        let decoder = JSONDecoder()
         
-        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
-            petitions = jsonPetitions.results
-            tableView.reloadData()
-        }
+        showError()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -43,5 +48,71 @@ class ViewController: UITableViewController {
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+            savePetitions = petitions
+        }
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func promptForInfo() {
+        let ac = UIAlertController(title: "Data Info", message: "The data comes from the We The People API of the Whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    @objc func filteredPetitions() {
+        let ac = UIAlertController(title: "Find your petitions", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let findAction = UIAlertAction(title: "Find", style: .default) { _ in
+            guard let text = ac.textFields?[0].text else { return }
+            self.findPetitions(text)
+        }
+        
+        let backAction = UIAlertAction(title: "Reset All", style: .destructive) { _ in
+            self.resetButton()
+        }
+        
+        ac.addAction(findAction)
+        ac.addAction(backAction)
+        present(ac, animated: true)
+    }
+    
+    func findPetitions(_ text: String) {
+        filterPetitions.removeAll()
+        for petition in savePetitions {
+            if petition.body.lowercased().contains(text.lowercased()) {
+                filterPetitions.append(petition)
+            }
+        }
+        
+        if filterPetitions.isEmpty { return }
+        petitions = filterPetitions
+        tableView.reloadData()
+        
+    }
+    
+    func resetButton() {
+        petitions = savePetitions
+        tableView.reloadData()
+    }
+    
 }
 
